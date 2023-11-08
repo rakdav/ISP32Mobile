772db627ef51686286b5e21fd5f7238a5132872e
Gogs <gogs@fake.local>
2023-11-08 15:16:25 +0200

    orderactivity

--- Lifecycle/app/src/main/java/com/example/lifecycle/MainActivity.kt
+++ Lifecycle/app/src/main/java/com/example/lifecycle/MainActivity.kt
@@ -1,22 +1,60 @@
 package com.example.lifecycle
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var expession:TextView
+    private lateinit var watch:Button
+    private lateinit var next:Button
+    private var x:Int = 0
+    private var y:Int = 0
+    private var op:Char = '\u0000'
     override fun onCreate(savedInstanceState: Bundle?) {
         Toast.makeText(this,"onCreate",Toast.LENGTH_SHORT).show()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        expession=findViewById(R.id.expression)
+        watch=findViewById(R.id.watch)
+        next=findViewById(R.id.next)
     }
 
     override fun onStart() {
         Toast.makeText(this,"onStart",Toast.LENGTH_SHORT).show()
         super.onStart()
-
+        next.visibility=View.INVISIBLE
+        x=Random().nextInt(10) + 1
+        y=Random().nextInt(10)+1
+        var operation:Int=Random().nextInt(3)+1
+        when(operation)
+        {
+            1 -> op='+'
+            2 -> op='-'
+            3 -> op='*'
+        }
+        expession.text="${x}${op}${y}"
+        watch.setOnClickListener {
+            var intent=Intent(applicationContext,ResultActivity::class.java)
+            intent.putExtra("x",x)
+            intent.putExtra("y",y)
+            when(op)
+            {
+                '+'->intent.putExtra("op",1)
+                '-'->intent.putExtra("op",2)
+                '*'->intent.putExtra("op",3)
+            }
+            startActivity(intent)
+        }
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onResume() {
         Toast.makeText(this,"onResume",Toast.LENGTH_SHORT).show()
         super.onResume()
---
+++ Lifecycle/app/src/main/java/com/example/lifecycle/ResultActivity.kt
@@ -0,0 +1,65 @@
+package com.example.lifecycle
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.RadioButton
+import java.util.Random
+
+class ResultActivity : AppCompatActivity() {
+    private var res:Int=0
+    private lateinit var first:RadioButton
+    private lateinit var second:RadioButton
+    private lateinit var third:RadioButton
+    private var result:Boolean=false
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+        first=findViewById(R.id.first)
+        second=findViewById(R.id.second)
+        third=findViewById(R.id.third)
+        if(intent.extras!=null)
+        {
+            var x=intent.getIntExtra("x",0)
+            var y=intent.getIntExtra("y",0)
+            var op=intent.getIntExtra("op",0)
+            when(op)
+            {
+                1->res=x+y
+                2->res=x-y
+                3->res=x*y
+            }
+            first.text=(Random().nextInt(20)-10).toString();
+            second.text=(Random().nextInt(20)-10).toString();
+            third.text=(Random().nextInt(20)-10).toString();
+            var position=Random().nextInt(3)+1
+            when(position)
+            {
+                1->first.text=res.toString()
+                2->second.text=res.toString()
+                3->third.text=res.toString()
+            }
+            first.setOnClickListener {
+                if(position==1) result=true
+                var data=Intent()
+                data.putExtra("result",result)
+                setResult(RESULT_OK,data)
+            }
+            second.setOnClickListener {
+                if(position==2) result=true
+                var data=Intent()
+                data.putExtra("result",result)
+                setResult(RESULT_OK,data)
+            }
+            third.setOnClickListener {
+                if(position==3) result=true
+                var data=Intent()
+                data.putExtra("result",result)
+                setResult(RESULT_OK,data)
+            }
+
+        }
+    }
+}
