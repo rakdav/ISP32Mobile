305e9873098b70bf36d6d6e490f90439e4c4a476
Gogs <gogs@fake.local>
2023-11-22 18:15:51 +0200

    lab4adapter

---
+++ Lab4/app/src/main/java/com/example/lab4/EventAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.lab4
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class EventAdapter(private val events:List<Event>): RecyclerView.Adapter<EventAdapter.EventViewHolder>() {
+    class EventViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val image: ImageView = itemView.findViewById(R.id.image)
+        val name: TextView = itemView.findViewById(R.id.etName)
+        val data:TextView=itemView.findViewById(R.id.etDate)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventViewHolder
+    {
+        val itemView =
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.event_item, parent, false)
+        return EventViewHolder(itemView)
+    }
+    override fun getItemCount(): Int {
+        return events.size
+    }
+    override fun onBindViewHolder(holder: EventViewHolder, position: Int) {
+        holder.name.text = events[position].name
+        holder.data.text = events[position].data.toString()
+    }
+}
