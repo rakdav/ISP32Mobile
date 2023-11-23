326c0bb633c4e587e06affb83ac4318d3b65b95d
Gogs <gogs@fake.local>
2023-11-23 15:06:54 +0200

    lastTest

--- TestProjectISP32/app/src/androidTest/java/com/example/testprojectisp32/ExampleInstrumentedTest.kt
+++ TestProjectISP32/app/src/androidTest/java/com/example/testprojectisp32/ExampleInstrumentedTest.kt
@@ -1,12 +1,19 @@
 package com.example.testprojectisp32
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.Espresso
+import androidx.test.rule.ActivityTestRule
+import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +22,15 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @get:Rule
+    var mActivityRule: ActivityTestRule<MainActivity> =
+        ActivityTestRule(MainActivity::class.java)
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.testprojectisp32", appContext.packageName)
+    public fun AddClick()
+    {
+        Espresso.onView(withId(R.id.var1)).perform(typeText("10"))
+        Espresso.onView(withId(R.id.var2)).perform(typeText("15"))
+        Espresso.onView(withId(R.id.add)).perform(click())
+        Espresso.onView(withId(R.id.result)).check(matches(withText("25")))
     }
 }
