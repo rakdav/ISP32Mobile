9234d3eb1575b9e70c5a5b6cbef55fa5e5b4f262
Gogs <gogs@fake.local>
2023-12-01 10:23:24 +0200

    eventActivity

--- Lab4/app/src/main/java/com/example/lab4/view/EventActivity.kt
+++ Lab4/app/src/main/java/com/example/lab4/view/EventActivity.kt
@@ -1,12 +1,51 @@
 package com.example.lab4.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
-import com.example.lab4.R
+import android.provider.MediaStore
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.example.lab4.databinding.ActivityEventBinding
 
 class EventActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEventBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_event)
+        binding = ActivityEventBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btCamera.setOnClickListener {
+            startForCamera.launch(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
+        }
+        binding.btGalary.setOnClickListener {
+            val takePictureIntent = Intent(Intent.ACTION_PICK)
+            takePictureIntent.type = "image/*"
+            startForGalary.launch(takePictureIntent)
+        }
+        binding.btOK.setOnClickListener {
+
+        }
+    }
+    var startForCamera=registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result:ActivityResult->
+        if(result.resultCode== Activity.RESULT_OK){
+            val photo = result.data?.extras?.get("data") as Bitmap
+            binding.ImageEvent.setImageBitmap(photo)
+        }
+    }
+    var startForGalary=registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result:ActivityResult->
+        if(result.resultCode== Activity.RESULT_OK){
+            var intent=result.getData();
+            if (intent != null) {
+                intent.putExtra("content","galery")
+            };
+            var imageStream = intent?.getData()?.let { contentResolver.openInputStream(it) }
+            var selectedImage = BitmapFactory.decodeStream(imageStream)
+            binding.ImageEvent.setImageBitmap(selectedImage)
+        }
     }
 }
